dfddb8597595c140dba584e1b54db9ada6e199bb
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-18 22:01:40 +0100

    minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery 
     before_action :authenticate_tenant!  
-
-    puts "/////////////////////////////////////////////////////////////////////////////////////////////////////////////"
-    puts "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  puts "SENDGRID_USERNAME: #{ENV['SENDGRID_USERNAME']}"
-  puts "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
 end
