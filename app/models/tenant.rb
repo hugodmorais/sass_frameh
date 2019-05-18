31fec753ca8a0cfac8c6b5596acbd2bef56014dc
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-18 20:45:00 +0100

    minor update

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,10 +1,17 @@
-class Tenant < ApplicationRecord
-
-   acts_as_universal_and_determines_tenant
+class Tenant < ActiveRecord::Base
+  
+  acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
+  has_many :projects, dependent: :destroy
+  has_one :payment
+  accepts_nested_attributes_for :payment
+  
+  def can_create_projects?
+    (plan == 'free' && projects.count < 1) || (plan == 'premium')  
+  end
+  
   validates_uniqueness_of :name
   validates_presence_of :name
-
     def self.create_new_tenant(tenant_params, user_params, coupon_params)
 
       tenant = Tenant.new(tenant_params)
